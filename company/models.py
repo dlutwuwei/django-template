349a2c8ad6893bd81d21d45c5ffa4b7c14b32c8a
wuwei <wuwei@bytedance.com>
2017-10-31 11:21:22 +0800

    add unique for some table field

--- company/models.py
+++ company/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 
 
 class School(models.Model):
-    school_name = models.CharField('分校名称', max_length=200)
+    school_name = models.CharField('分校名称', max_length=200, unique=True)
     school_master = models.CharField('分校负责人', max_length=200, null=True, blank=True)
     def __str__(self):
       return self.school_name
@@ -27,7 +27,7 @@ class Company(models.Model):
       verbose_name="填报人",
       on_delete=models.CASCADE
     )
-    company_name = models.CharField('分公司名称', max_length=200)
+    company_name = models.CharField('分公司名称', max_length=200, unique=True)
     company_code = models.CharField('分公司代码', max_length=6, unique=True)
     def __str__(self):
       return self.company_name
@@ -49,7 +49,7 @@ class Employee(models.Model):
 
 
 class ExamItem(models.Model):
-    item_name = models.CharField('考试项目', max_length=100)
+    item_name = models.CharField('考试项目', max_length=100, unique=True)
     class Meta:
       verbose_name='考试项目'
       verbose_name_plural = '考试项目设置'
@@ -62,7 +62,7 @@ class ExamDetailItem(models.Model):
       on_delete=models.CASCADE,
       verbose_name="所属项目",
     )
-    detail_name = models.CharField('考试明细项目', max_length=100)
+    detail_name = models.CharField('考试明细项目', max_length=100, unique=True)
     class Meta:
       verbose_name='考试明细项目'
       verbose_name_plural = '考试明细项目设置'
@@ -70,7 +70,7 @@ class ExamDetailItem(models.Model):
       return self.detail_name
 
 class ExamType(models.Model):
-    type_name = models.CharField('考试类型', max_length=100)
+    type_name = models.CharField('考试类型', max_length=100, unique=True)
     class Meta:
       verbose_name='考试类型'
       verbose_name_plural = '考试类型设置'
@@ -78,7 +78,7 @@ class ExamType(models.Model):
       return self.type_name
 
 class ClassType(models.Model):
-    class_name = models.CharField('班型', max_length=100)
+    class_name = models.CharField('班型', max_length=100, unique=True)
     class Meta:
       verbose_name='班型'
       verbose_name_plural = '班型设置'
